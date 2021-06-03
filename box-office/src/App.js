022354ac77eb1f2b24309cbae535657c792db996
chiragmahajan146 <chiragmahajan146@gmail.com>
2021-06-03 22:36:57 +0530

    added nav-bar

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
 
 function App() {
-  return <div>dii</div>;
+  return (
+    <div>
+      <Navs />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>404 page not found</Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
